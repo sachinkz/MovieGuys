36c8cd71e3692d2291db241551f52d0fbd776396
sachin-kx <oggypubgaming@gmail.com>
2024-06-07 08:37:38 +0530

    edited main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,12 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
         <GoogleOAuthProvider clientId='437456577968-k8t4aij6l0s1rert4o37cppnkk9smseu.apps.googleusercontent.com'>
           <App />
         </GoogleOAuthProvider>
-      </AuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 )
