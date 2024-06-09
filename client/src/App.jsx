a9b783d091b4fd7fcda32a3699e7da75928abba8
sachin-kx <oggypubgaming@gmail.com>
2024-06-09 09:13:06 +0530

    lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import { LoaderIcon } from 'lucide-react';
 
-import Home from './pages/Home';
-import LogIn from './pages/Login';
-import Signup from './pages/Signup';
-import CreatePost from './pages/CreatePost';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar';
-import EditPost from './pages/EditPost';
-// const Home = lazy(() => import('./pages/Home'));
-// const LogIn = lazy(() => import('./pages/Login'));
-// const Signup = lazy(() => import('./pages/Signup'));
-// const CreatePost = lazy(() => import('./pages/CreatePost'));
-// const Profile = lazy(() => import('./pages/Profile'));
-// const Navbar = lazy(() => import('./components/Navbar'));
-// const EditPost = lazy(() => import('./pages/EditPost'));
+const Home = lazy(() => import('./pages/Home'));
+const LogIn = lazy(() => import('./pages/Login'));
+const Signup = lazy(() => import('./pages/Signup'));
+const CreatePost = lazy(() => import('./pages/CreatePost'));
+const Profile = lazy(() => import('./pages/Profile'));
+const Navbar = lazy(() => import('./components/Navbar'));
+const EditPost = lazy(() => import('./pages/EditPost'));
 
 function App() {
   const { user } = useAuth();
@@ -47,9 +40,13 @@ function App() {
   }
 
   return (
-    <>
+    <Suspense fallback={
+      <div className='w-full h-screen flex justify-center items-center'>
+         <LoaderIcon className="h-14 w-14 animate-spin text-white" />
+      </div>
+    }>
       {routes}
-    </>
+    </Suspense>
   );
 }
 
